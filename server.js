63a0c5568fddb4b42422af1e016b8a058be5d114
Juan Antonio Campano Berlanga <juan.a.campano@gmail.com>
2024-12-07 14:35:45 +0100

    Modificaciones

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const tasksRoutes = require('./routes/tasks');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +11,11 @@ app.use(bodyParser.json());
 
 // Rutas
 app.use('/api/tasks', tasksRoutes);
+
+// Ruta para el WADL
+app.get('/api/tasks.wadl', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'tasks.wadl'));
+});
 
 // Iniciar el servidor
 app.listen(PORT, () => {
